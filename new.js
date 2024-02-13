031a1e7308b9f06bdf334c11ebb1d284b98b136e
westzer001 <westzer0@amuz.co.kr>
2024-02-13 18:12:15 +0900

    필기 내용 추가 new.js

--- new.js
+++ new.js
@@ -47,6 +47,8 @@ function filterProducts(isChecked) {
     displayProducts(filteredProducts);
 }
 
+// filterProducts 배열에 있는 각 제품에 대해 createProductCard 함수를 호출하여 제품 카드를 생성
+// 그 결과를 이전에 선언해두었던 #product-card 요소에 추가함
 function displayProducts(filteredProducts) {
     var productCardElement = document.querySelector('#product-card');
     productCardElement.innerHTML = '';
@@ -57,8 +59,10 @@ function displayProducts(filteredProducts) {
 }
 
 // JSON 데이터를 가져오는 함수
+// json 파일을 불러오고 그 결과를 파싱하여 data 변수에 저장 후
+// 함수 호출을 하여 모든 상품을 표시하는 역활
 function loadData() {
-    return fetch('new.json')
+    return fetch('new.json') //fetch는 new.json을 반환하는 역활을 가진다.
         .then(response => response.json())
         .then(jsonData => {
             data = Object.values(jsonData);  // JSON 데이터를 `data` 변수에 저장
@@ -68,6 +72,8 @@ function loadData() {
 }
 
 // 이벤트 리스너 함수
+// box라는 이름의 class를 가진 HTML 요소에 change 이벤트 리스너를 등록하는 역활
+// 해당 이벤트가 발생하면, 해당 요소의 체크 상태를 확인하고, 결과에 따라 filterProducts 함수 호출
 function registerEventListener() {
     document.querySelector('.box').addEventListener('change', function() {
         var isChecked = this.checked;
